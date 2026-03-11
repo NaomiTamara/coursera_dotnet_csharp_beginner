cb901cf9c89b01e2b0059d7983fbae0715835805
Naomi Voerman <n.voerman@qii.nl>
2026-03-11 12:45:08 +0100

    swagger client api

--- SwaggerApiClientLab/Controllers/UserController.cs
+++ SwaggerApiClientLab/Controllers/UserController.cs
@@ -1,6 +1,27 @@
-namespace DefaultNamespace;
+// Controllers/UserController.cs
+using Microsoft.AspNetCore.Mvc;
 
-public class UserController
+// User model
+public class User
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet("{id}")]
+    [Produces("application/json")]
+    public ActionResult<User> GetUser(int id)
+    {
+        var user = new User 
+        { 
+            Id = id,
+            Name = $"User {id}"
+        };
+        
+        return Ok(user);
+    }
 }
--- SwaggerApiClientLab/Program.cs
+++ SwaggerApiClientLab/Program.cs
@@ -1,26 +1,29 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿// Program.cs
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
-using System.Threading.Tasks;
-using MyClientNamespace;
+using MyApiClientNamespace;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        var app = builder.Build();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
-        app.MapControllers();
+		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddControllers();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
 
-        var httpClient = new HttpClient();
-        var client = new CustomApiClient("http://localhost:5000", httpClient);
+		var app = builder.Build();
 
-        var user = await client.UserAsync(1);
-        Console.WriteLine($"Fetched User: {user}");
+		app.UseSwagger();
+		app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+		
+		app.MapControllers();
+		Task.Run(() => app.RunAsync());
+		var httpClient = new HttpClient();
+		var client = new GeneratedApiClient("http://localhost:5000", httpClient);
+		
+		var user = await client.GetUserAsync(1);
+
+		app.Run();
     }
 }
